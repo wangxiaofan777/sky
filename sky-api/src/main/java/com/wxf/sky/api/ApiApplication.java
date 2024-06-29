97cd4c9cdcab3a17ea7111cf03a7bb44bbb95216
wangmaosong <wangmsd@dcits.com>
2024-06-29 13:05:39 +0800

    feat：init

---
+++ sky-api/src/main/java/com/wxf/sky/api/ApiApplication.java
@@ -0,0 +1,18 @@
+package com.wxf.sky.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * API 入口
+ *
+ * @author Wxf
+ * @since 2024-06-29 12:54:44
+ **/
+@SpringBootApplication
+public class ApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiApplication.class, args);
+    }
+}
