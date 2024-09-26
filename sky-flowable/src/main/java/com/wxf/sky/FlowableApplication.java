8dbb1460dd768213ff75f6c93bb7d537763eb41d
wangmaosong <wangmsd@dcits.com>
2024-09-26 14:03:43 +0800

    feat: 添加 新module

---
+++ sky-flowable/src/main/java/com/wxf/sky/FlowableApplication.java
@@ -0,0 +1,16 @@
+package com.wxf.sky;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Wxf
+ * @since 2024-09-26 11:11:42
+ **/
+@SpringBootApplication
+public class FlowableApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlowableApplication.class, args);
+    }
+}
